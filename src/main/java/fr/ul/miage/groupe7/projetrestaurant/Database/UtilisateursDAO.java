aa5998966b68eb1c3d6d5bf3279bad8592795a9c
LordOfRaptor <quentin.noir@gmail.com>
2021-04-24 18:39:43 +0200

    US005 recupere info utilisateur + test

--- src/main/java/fr/ul/miage/groupe7/projetrestaurant/Database/UtilisateursDAO.java
+++ src/main/java/fr/ul/miage/groupe7/projetrestaurant/Database/UtilisateursDAO.java
@@ -1,24 +1,36 @@
 package fr.ul.miage.groupe7.projetrestaurant.Database;
 
-public class UtilisateursDAO extends DAO<UtilisateursDAO> {
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.*;
+
+public class UtilisateursDAO extends DAO<Utilisateurs> {
+
+
+    public UtilisateursDAO(){
+        super("Utilisateurs");
+    }
 
     @Override
-    public UtilisateursDAO find(String id) {
+    public Utilisateurs find(String... id) {
+        Document d = connect.find(and(eq("identifiant",id[0]),eq("mdp",id[1]))).first();
+
+        return (d == null) ? null
+                : new Utilisateurs(d);
+    }
+
+    @Override
+    public Utilisateurs create(Utilisateurs obj) {
         return null;
     }
 
     @Override
-    public UtilisateursDAO create(UtilisateursDAO obj) {
+    public Utilisateurs update(Utilisateurs obj) {
         return null;
     }
 
     @Override
-    public UtilisateursDAO update(UtilisateursDAO obj) {
-        return null;
-    }
-
-    @Override
-    public void delete(UtilisateursDAO obj) {
+    public void delete(Utilisateurs obj) {
 
     }
 }
