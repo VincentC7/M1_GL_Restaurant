d7161c8c35a4339701ae59d079bd3b65d3d7abdc
Tristan54 <tristan.luc1@etu.univ-lorraine.fr>
2021-04-25 16:17:23 +0200

    ft + test (service): Ajout du sytème de configuration
    Permet de charger par un système de clé/valeur des configurations tel que le nom de la base de donnée par exemple.
    
    Close US003

---
+++ src/main/java/fr/ul/miage/groupe7/projetrestaurant/service/GeneralProperties.java
@@ -0,0 +1,66 @@
+package fr.ul.miage.groupe7.projetrestaurant.service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GeneralProperties {
+
+    private static  Properties prop;
+
+    /**
+     * Permet de charger le fichier de paramétrage de l'application
+     *
+     */
+    private static void load(){
+        try (InputStream input = new FileInputStream("config.properties")) {
+
+            prop = new Properties();
+            prop.load(input);
+
+        } catch (IOException ex) {
+            System.err.println("Fichier de configuration introuvable");
+        }
+    }
+
+    /**
+     * Permet de charger le fichier de paramétrage de l'application
+     *
+     * @param fileName Nom du fichier de configuration à charger
+     */
+    private static void load(String fileName){
+        try (InputStream input = new FileInputStream(fileName)) {
+
+            prop = new Properties();
+            prop.load(input);
+
+        } catch (IOException ex) {
+            System.err.println("Fichier de configuration introuvable");
+        }
+    }
+
+    /**
+     *  Retourne la valeur qui correspond au nom du paramètre de configuration que l'on passe en paramètre de la fonction
+     *
+     * @param propertieName Nom du paramètre auquel on souhaite accéder
+     * @return
+     */
+    public static String getPropertie(String propertieName){
+        load();
+        return prop.getProperty(propertieName);
+    }
+
+    /**
+     *  Retourne la valeur qui correspond au nom du paramètre de configuration que l'on passe en paramètre de la fonction
+     *
+     * @param propertieName Nom du paramètre auquel on souhaite accéder
+     * @param fileName Nom du fichier de configuration
+     * @return
+     */
+    public static String getPropertie(String propertieName, String fileName){
+        load(fileName);
+        return prop.getProperty(propertieName);
+    }
+
+}
---
+++ src/test/java/fr/ul/miage/groupe7/projetrestaurant/PropertiesTest.java
@@ -0,0 +1,32 @@
+package fr.ul.miage.groupe7.projetrestaurant;
+
+import fr.ul.miage.groupe7.projetrestaurant.service.GeneralProperties;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PropertiesTest {
+
+
+    @Test
+    @DisplayName("Récuparation de la bonne valeur")
+    public void getValue(){
+        assertEquals("restaurant", GeneralProperties.getPropertie("DATABASE_NAME"));
+    }
+
+    @Test
+    @DisplayName("Exception quand le fichier n'existe pas")
+    public void loadFailed(){
+        assertThrows(Exception.class, () -> {
+            GeneralProperties.getPropertie("DATABASE_NAME", "config");
+        });
+    }
+
+    @Test
+    @DisplayName("Retroune null quand la clé n'existe pas")
+    public void missing_key(){
+        assertNull(GeneralProperties.getPropertie("bidon"));
+    }
+}
