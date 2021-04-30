7d19b54d81aac5202c4f394cc4d884ca2d8d0841
LordOfRaptor <quentin.noir@gmail.com>
2021-04-30 15:41:48 +0200

    fix (test): Ajout d'un mode test pour la BDD
    Ajoute d'un moyen pour avoir une base de donnée de test

--- src/main/java/fr/ul/miage/groupe7/projetrestaurant/Database/BDD_Connexion.java
+++ src/main/java/fr/ul/miage/groupe7/projetrestaurant/Database/BDD_Connexion.java
@@ -3,15 +3,22 @@ package fr.ul.miage.groupe7.projetrestaurant.Database;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import fr.ul.miage.groupe7.projetrestaurant.service.GeneralProperties;
 
 public class BDD_Connexion {
 
     private static BDD_Connexion bdd_connexion;
     private static MongoDatabase database;
+    private static boolean test = false;
+    private final static String DATABASE_NAME="DATABASE_NAME";
+    private final static String DATABASE_TEST="DATABASE_TEST";
 
     private BDD_Connexion(){
         MongoClient mongoClient = MongoClients.create();
-        database = mongoClient.getDatabase("restaurant");
+        if(!test)
+            database = mongoClient.getDatabase(GeneralProperties.getPropertie(DATABASE_NAME));
+        else
+            database = mongoClient.getDatabase(GeneralProperties.getPropertie(DATABASE_TEST));
     }
 
     public static MongoDatabase getInstance(){
@@ -19,5 +26,9 @@ public class BDD_Connexion {
             bdd_connexion = new BDD_Connexion();
         }
         return database;
+    }
+
+    public static void setTest(){
+        test = true;
     }
 }
